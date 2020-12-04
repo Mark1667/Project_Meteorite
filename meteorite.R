3baf6a37c44c36ef33740436a7f400c5fa812ed8
Mark Hamilton <74188956+Mark1667@users.noreply.github.com>
2020-12-04 20:38:10 +0000

    first commit meteorite clean started

---
+++ meteorite.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(janitor)
+
+meteorite_data <-read_csv(("meteorite_landings.csv"))
+
+meteorite_clean <- janitor::clean_names(meteorite_data)
+
+meteorite_clean
+
+
+meteorite_clean <- meteorite_clean %>%  
+  separate(
+  col = geo_location,
+  into = c("latitude", "longitude"), 
+  sep = "\\,"
+  )
+
+meteorite_clean
+
+meteorite_cleans <- meteorite_clean %>% 
+  mutate(
+  longitude = str_remove(longitude, pattern = "[)]"))
+
+
+meteorite_cleans <- meteorite_clean %>% 
+  mutate(
+    latitude  = str_remove(latitude, pattern = "[(]"))
+  
+meteorite_cleans
+
+meteorite_cleans %>% 
+  summarise( na_latitude = sum(is.na(latitude)),
+             na_longitude = sum(is.na(longitude)))
+  
+meteorite_cleans %>% 
+  filter
+
