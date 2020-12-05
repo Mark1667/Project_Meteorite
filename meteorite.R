e16f20881f8846bcbd4e4aef210667016d887b65
Mark Hamilton <74188956+Mark1667@users.noreply.github.com>
2020-12-05 18:11:44 +0000

    na's changed to zeros

--- meteorite.R
+++ meteorite.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(janitor)
+library(dplyr)
 
 meteorite_data <-read_csv(("meteorite_landings.csv"))
 
@@ -19,18 +20,27 @@ meteorite_clean
 
 meteorite_cleans <- meteorite_clean %>%
   mutate(
-  longitude = str_remove(longitude, pattern = "[)]"))
-
-
-meteorite_cleans <- meteorite_clean %>%
+  longitude = str_remove(longitude, pattern = "[)]")) %>% 
   mutate(
     latitude  = str_remove(latitude, pattern = "[(]"))
 
-meteorite_cleans
 
-meteorite_cleans %>%
-  summarise( na_latitude = sum(is.na(latitude)),
-             na_longitude = sum(is.na(longitude)))
 
-meteorite_cleans %>%
-  filterdsads
+
+meteorite_cleans_numeric <- meteorite_cleans%>%
+  mutate(
+    longitude = as.numeric(longitude)) %>% 
+  mutate(
+    latitude  = as.numeric(latitude))
+
+
+  meteorite_cleans_numeric %>% 
+  summarise(across(.fns = ~sum(is.na(.x))))
+
+  meteorite_cleans_numeric <- meteorite_cleans_numeric %>% 
+  replace(is.na(.), 0)
+ 
+  
+  meteorite_cleans_numeric %>% 
+    summarise(across(.fns = ~sum(is.na(.x))))
+  
